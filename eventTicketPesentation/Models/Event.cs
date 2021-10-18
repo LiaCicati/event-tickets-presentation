32716ee483ffd7fe6640390afb330660ce5e21b3
Lia Cicati <LiaCicati@yandex.com>
2021-10-18 11:51:19 +0200

    made use of aspnetcore components forms

--- eventTicketPesentation/Models/Event.cs
+++ eventTicketPesentation/Models/Event.cs
@@ -1,20 +1,27 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace eventTicketPesentation.Models
 {
     public class Event
     {
         public long id { get; set; }
+
+        [Required(ErrorMessage = "Name Field is Required")]
         public string name { get; set; }
+
+        [Required(ErrorMessage = "Description Field is Required")]
         public string description { get; set; }
+
         public string location { get; set; }
         public string thumbnail { get; set; }
         public int nrOfTickets { get; set; }
         public string Category { get; set; }
         public bool isCancelled { get; set; }
         public DateTime dateTime { get; set; }
-    
-        public override string ToString() {
+
+        public override string ToString()
+        {
             return $"{id} {name} {description}";
         }
     }
