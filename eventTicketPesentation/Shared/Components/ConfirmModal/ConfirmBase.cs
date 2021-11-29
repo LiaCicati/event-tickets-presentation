c7379fe1040eefb4fa6bf88764f37a1dcff9fe12
Lia Cicati <LiaCicati@yandex.com>
2021-11-29 20:34:11 +0100

    added : confirm modal - cancel event

---
+++ eventTicketPesentation/Shared/Components/ConfirmModal/ConfirmBase.cs
@@ -0,0 +1,31 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace eventTicketPesentation.Shared.Components
+{
+    public class ConfirmBase : ComponentBase
+    {
+        protected bool ShowConfirmation { get; set; }
+
+        [Parameter]
+        public string ConfirmationTitle { get; set; } = "Confirm Action";
+
+        [Parameter]
+        public string ConfirmationMessage { get; set; } = "Are you sure you want to cancel the event?";
+
+        public void Show()
+        {
+            ShowConfirmation = true;
+            StateHasChanged();
+        }
+
+        [Parameter]
+        public EventCallback<bool> ConfirmationChanged { get; set; }
+
+        protected async Task OnConfirmationChange(bool value)
+        {
+            ShowConfirmation = false;
+            await ConfirmationChanged.InvokeAsync(value);
+        }
+    }
+}
